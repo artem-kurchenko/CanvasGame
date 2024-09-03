99ba9d1a5c9bb45f63123e939f641b21cb5f1400
Artem K <artem.kurchenko@devexpress.com>
2024-09-03 13:22:20 +0400

    Implement forwardRef for Game

--- duel-game-app/src/components/GameComponent.tsx
+++ duel-game-app/src/components/GameComponent.tsx
@@ -1,21 +1,30 @@
-import react, { useEffect, useRef } from 'react';
+import { forwardRef, memo, useImperativeHandle, useRef } from 'react';
 import { DuelGame } from '../game_core/game';
+import './GameComponent.css';
 
-export function GameComponent(): JSX.Element {
-    const gameContainer = useRef<HTMLDivElement>(null)
-    const gameInstance = useRef<DuelGame | null>(null);
-    useEffect(() => {
-        if (gameInstance.current)
-            return
-        gameInstance.current = new DuelGame({ container: gameContainer.current, width: 400, height: 300 });
-        gameInstance.current.render({ radius: 15, color: 'green' }, { radius: 15, color: 'yellow' });
-        gameInstance.current.start();
-        setTimeout(() => {
-            gameInstance.current?.stop();
-        }, 20000)
-    }, [gameContainer, gameInstance])
+interface GameComponentProps {
+    handleContextMenu: (e: React.MouseEvent) => void;
+    changeScoreCallback: ()=>void;
+    width?: number;
+    height?: number;
+};
 
-    return (<div ref={gameContainer}></div>)
-}
+const GameComponent = forwardRef<DuelGame, GameComponentProps>((props, ref) => {
+    const { handleContextMenu, changeScoreCallback, width, height } = props;
+    const innerRef = useRef<DuelGame>();
+
+    useImperativeHandle(ref, () => innerRef.current as DuelGame)
+
+    const handleRef = (node: HTMLDivElement) => {
+        if(innerRef.current)
+             return;
+        innerRef.current = new DuelGame({ container: node, width: width || 400,  height: height || 300, className :"game-container", changeScoreCallback  });
+        innerRef.current.render({ radius: 15, color: 'green' }, { radius: 15, color: 'yellow' });
+    };
+
+    return (<div onContextMenu={handleContextMenu} ref={handleRef}></div>)
+});
+
+export default memo(GameComponent);
 
 
